dab45ab7a827d5b00ced5695b3faab7f996d5289
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-09-22 23:15:32 +0700

    feat: add Nebula locking; reformat Java files; add .editorconfig

--- src/main/java/com/countries/database/demo/CountriesController.java
+++ src/main/java/com/countries/database/demo/CountriesController.java
@@ -4,80 +4,73 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 public class CountriesController {
-  @Autowired
-  CountryService countryService;
+    @Autowired
+    CountryService countryService;
 
-  @GetMapping(path="/getCountries")
-  public ResponseEntity<List> getCountries() {
-    log.info("[FLO] /getCountries is called");
-    try {
-      return new ResponseEntity<>(countryService.getCountries(), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    @GetMapping(path = "/getCountries")
+    public ResponseEntity<List> getCountries() {
+        log.info("[FLO] /getCountries is called");
+        try {
+            return new ResponseEntity<>(countryService.getCountries(), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
-  }
 
-  @GetMapping(path="/getCountries/{countryId}")
-  public ResponseEntity<Country> getCountriesWithId(@PathVariable Integer countryId) {
-    log.info("[FLO] /getCountries/{countryId} is called");
-    try {
-      return new ResponseEntity<>(countryService.getCountriesWithId(countryId), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    @GetMapping(path = "/getCountries/{countryId}")
+    public ResponseEntity<Country> getCountriesWithId(@PathVariable Integer countryId) {
+        log.info("[FLO] /getCountries/{countryId} is called");
+        try {
+            return new ResponseEntity<>(countryService.getCountriesWithId(countryId), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
-  }
 
-  @GetMapping(path="/getCountries/countryName")
-  public ResponseEntity<Country> getCountriesWithName(@RequestParam(value="name") String countryName) {
-    log.info("[FLO] /getCountries/countryName is called");
-    try {
-      return new ResponseEntity<>(countryService.getCountriesWithName(countryName), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    @GetMapping(path = "/getCountries/countryName")
+    public ResponseEntity<Country> getCountriesWithName(@RequestParam(value = "name") String countryName) {
+        log.info("[FLO] /getCountries/countryName is called");
+        try {
+            return new ResponseEntity<>(countryService.getCountriesWithName(countryName), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
-  }
 
-  @PostMapping(path="addCountry")
-  public ResponseEntity<Country> addCountry(@RequestBody Country addedCountry) {
-    log.info("[FLO] /addCountry is called");
-    try {
-      return new ResponseEntity<>(countryService.addCountry(addedCountry), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    @PostMapping(path = "addCountry")
+    public ResponseEntity<Country> addCountry(@RequestBody Country addedCountry) {
+        log.info("[FLO] /addCountry is called");
+        try {
+            return new ResponseEntity<>(countryService.addCountry(addedCountry), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
-  }
 
-  @PutMapping(path="updateCountry")
-  public ResponseEntity<Country> updateCountry(@RequestBody Country updatedCountry) {
-    log.info("[FLO] /updateCountry is called");
-    try {
-      return new ResponseEntity<>(countryService.updateCountry(updatedCountry), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    @PutMapping(path = "updateCountry")
+    public ResponseEntity<Country> updateCountry(@RequestBody Country updatedCountry) {
+        log.info("[FLO] /updateCountry is called");
+        try {
+            return new ResponseEntity<>(countryService.updateCountry(updatedCountry), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
-  }
 
-  @DeleteMapping(path="deleteCountry/{countryId}")
-  public ResponseEntity<ResponseMessage> deleteCountry(@PathVariable Integer countryId) {
-    log.info("[FLO] deleteCountry/{countryId} is called");
-    try {
-      return new ResponseEntity<>(countryService.deleteCountry(countryId), HttpStatus.OK);
-    } catch (Exception e) {
-      return new ResponseEntity<>(HttpStatus.CONFLICT);
+    @DeleteMapping(path = "deleteCountry/{countryId}")
+    public ResponseEntity<ResponseMessage> deleteCountry(@PathVariable Integer countryId) {
+        log.info("[FLO] deleteCountry/{countryId} is called");
+        try {
+            return new ResponseEntity<>(countryService.deleteCountry(countryId), HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
     }
-  }
 }
--- src/main/java/com/countries/database/demo/CountryService.java
+++ src/main/java/com/countries/database/demo/CountryService.java
@@ -9,45 +9,46 @@ import java.util.List;
 @Service
 @Slf4j
 public class CountryService {
-  @Autowired(required = false)
-  CountryRepository countryRepository;
-  public List<Country> getCountries() {
-    return (List<Country>) countryRepository.findAll();
-  }
+    @Autowired(required = false)
+    CountryRepository countryRepository;
 
-  public Country getCountriesWithId(Integer countryId) {
-    return countryRepository.findById(countryId).get();
-  }
-
-  public Country getCountriesWithName(String countryName) {
-    List<Country> countryList = (List<Country>) countryRepository.findAll();
-    for (Country country:countryList) {
-      if (countryName.equals(country.getCountryName())) {
-        return country;
-      }
+    public List<Country> getCountries() {
+        return (List<Country>) countryRepository.findAll();
     }
-    return new Country(0,"NONE","NONE");
-  }
 
-  public Country addCountry(Country addedCountry) {
-    long size = countryRepository.count();
-    log.info("[FLO] REPOSITORY SIZE IS: " + size);
-    Integer newId = (int) size + 1;
-    log.info("[FLO] newId IS: " + newId);
-    Country country = new Country(newId, addedCountry.getCountryName(), addedCountry.getCapital());
-    log.info("[FLO] newly added country is: " + country.getId() + country.getCountryName() + country.getCapital());
-    countryRepository.save(country);
-    log.info("[FLO] newly added country in repository is: " + countryRepository.findById(newId).get());
-    return countryRepository.findById(newId).get();
-  }
+    public Country getCountriesWithId(Integer countryId) {
+        return countryRepository.findById(countryId).get();
+    }
 
-  public Country updateCountry(Country updatedCountry) {
-    countryRepository.save(updatedCountry);
-    return countryRepository.findById(updatedCountry.getId()).get();
-  }
+    public Country getCountriesWithName(String countryName) {
+        List<Country> countryList = (List<Country>) countryRepository.findAll();
+        for (Country country : countryList) {
+            if (countryName.equals(country.getCountryName())) {
+                return country;
+            }
+        }
+        return new Country(0, "NONE", "NONE");
+    }
 
-  public ResponseMessage deleteCountry(Integer countryId) {
-    countryRepository.deleteById(countryId);
-    return new ResponseMessage("Country deleted");
-  }
+    public Country addCountry(Country addedCountry) {
+        long size = countryRepository.count();
+        log.info("[FLO] REPOSITORY SIZE IS: " + size);
+        Integer newId = (int) size + 1;
+        log.info("[FLO] newId IS: " + newId);
+        Country country = new Country(newId, addedCountry.getCountryName(), addedCountry.getCapital());
+        log.info("[FLO] newly added country is: " + country.getId() + country.getCountryName() + country.getCapital());
+        countryRepository.save(country);
+        log.info("[FLO] newly added country in repository is: " + countryRepository.findById(newId).get());
+        return countryRepository.findById(newId).get();
+    }
+
+    public Country updateCountry(Country updatedCountry) {
+        countryRepository.save(updatedCountry);
+        return countryRepository.findById(updatedCountry.getId()).get();
+    }
+
+    public ResponseMessage deleteCountry(Integer countryId) {
+        countryRepository.deleteById(countryId);
+        return new ResponseMessage("Country deleted");
+    }
 }
