f14ac76c5659e076ca840a8a5d75fb650a5e8a28
florentin-a-p <florentin.purnama@gmail.com>
2022-10-05 02:54:34 +0700

    put all unhappy paths for mockmvc

--- src/test/java/com/example/demo/TestMockMvcCountriesController.java
+++ src/test/java/com/example/demo/TestMockMvcCountriesController.java
@@ -17,10 +17,12 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.system.CapturedOutput;
 import org.springframework.boot.test.system.OutputCaptureExtension;
+import org.springframework.dao.InvalidDataAccessResourceUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.transaction.CannotCreateTransactionException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,7 +40,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
-//@ContextConfiguration
 @SpringBootTest(classes={CountriesController.class})
 public class TestMockMvcCountriesController {
   @Autowired
@@ -79,6 +80,80 @@ public class TestMockMvcCountriesController {
     then(countryService).should(times(1)).getCountries();
     assertThat(output).contains("[FLO] /getCountries is called");
   }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void getCountriesDbExistTblNotExist_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    given(countryService.getCountries()).willAnswer( invocation -> { throw new InvalidDataAccessResourceUsageException(""); });
+
+    // When
+    MvcResult actualResponse = this.mockMvc.perform(get("/getCountries"))
+        .andExpect(status().isGone())
+        .andExpect(status().is4xxClientError())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] /getCountries is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
+  }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void getCountriesDbDeletedWhileSpringRunning_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    given(countryService.getCountries()).willAnswer( invocation -> { throw new CannotCreateTransactionException(""); });
+
+    // When
+    MvcResult actualResponse = this.mockMvc.perform(get("/getCountries"))
+        .andExpect(status().isExpectationFailed())
+        .andExpect(status().is4xxClientError())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] /getCountries is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
+  }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void getCountriesTableEmpty_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    List<Country> countryList = new ArrayList<Country>();
+    given(countryService.getCountries()).willReturn(countryList);
+
+    // When
+    MvcResult actualResponse = this.mockMvc.perform(get("/getCountries"))
+        .andExpect(status().isNoContent())
+        .andExpect(status().is2xxSuccessful())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] /getCountries is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
+  }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void getCountriesOtherException_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    given(countryService.getCountries()).willAnswer( invocation -> { throw new Exception(); });
+
+    // When
+    //ResponseEntity<List<Country>> actualResponse = countriesController.getCountries();
+    MvcResult actualResponse = this.mockMvc.perform(get("/getCountries"))
+        .andExpect(status().isIAmATeapot())
+        .andExpect(status().is4xxClientError())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] /getCountries is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
+  }
   // end region scenario for getCountries //
 
   // start region scenario for getCountriesWithId //
@@ -104,6 +179,27 @@ public class TestMockMvcCountriesController {
     then(countryService).should(times(1)).getCountriesWithId(countryId);
     assertThat(output).contains("[FLO] /getCountries/{countryId} is called");
   }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void getCountriesWithIdException_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    EasyRandom easyRandom = new EasyRandom();
+    Integer countryId = easyRandom.nextInt();
+    given(countryService.getCountriesWithId(countryId)).willAnswer( invocation -> { throw new Exception(); });
+
+    // When
+    //ResponseEntity<Country> actualResponse = countriesController.getCountriesWithId(countryId);
+    MvcResult actualResponse = this.mockMvc.perform(get("/getCountries/{countryId}",countryId))
+        .andExpect(status().isConflict())
+        .andExpect(status().is4xxClientError())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] /getCountries/{countryId} is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
+  }
   // end region scenario for getCountriesWithId //
 
   // start region scenario for getCountriesWithName //
@@ -128,6 +224,26 @@ public class TestMockMvcCountriesController {
     assertEquals(countryString,actualResponse.getResponse().getContentAsString());
     then(countryService).should(times(1)).getCountriesWithName(countryName);
     assertThat(output).contains("[FLO] /getCountries/countryName is called");
+  }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void getCountriesWithNameException_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    EasyRandom easyRandom = new EasyRandom();
+    String countryName = easyRandom.nextObject(String.class);
+    given(countryService.getCountriesWithName(countryName)).willAnswer( invocation -> { throw new Exception(); });
+
+    // When
+    MvcResult actualResponse = this.mockMvc.perform(get("/getCountries/countryName").param("name",countryName))
+        .andExpect(status().isConflict())
+        .andExpect(status().is4xxClientError())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] /getCountries/countryName is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
   }
   // end region scenario for getCountriesWithName //
 
@@ -159,6 +275,30 @@ public class TestMockMvcCountriesController {
     then(countryService).should(times(1)).addCountry(country);
     assertThat(output).contains("[FLO] /addCountry is called");
   }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void addCountryException_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    EasyRandom easyRandom = new EasyRandom();
+    Country country = easyRandom.nextObject(Country.class);
+    given(countryService.addCountry(country)).willAnswer( invocation -> { throw new Exception(); });
+    ObjectMapper mapper = new ObjectMapper();
+    String countryString = mapper.writeValueAsString(country);
+
+    // When
+    MvcResult actualResponse = this.mockMvc.perform(post("/addCountry")
+            .content(countryString)
+            .contentType("application/json;charset=UTF-8"))
+        .andExpect(status().isConflict())
+        .andExpect(status().is4xxClientError())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] /addCountry is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
+  }
   // end region scenario for addCountry //
 
   // start region scenario for updateCountry //
@@ -184,6 +324,30 @@ public class TestMockMvcCountriesController {
     assertEquals(countryString,actualResponse.getResponse().getContentAsString());
     then(countryService).should(times(1)).updateCountry(country);
     assertThat(output).contains("[FLO] /updateCountry is called");
+  }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void updateCountryException_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    EasyRandom easyRandom = new EasyRandom();
+    Country country = easyRandom.nextObject(Country.class);
+    given(countryService.updateCountry(country)).willAnswer( invocation -> { throw new Exception(); });
+    ObjectMapper mapper = new ObjectMapper();
+    String countryString = mapper.writeValueAsString(country);
+
+    // When
+    MvcResult actualResponse = this.mockMvc.perform(put("/updateCountry")
+            .content(countryString)
+            .contentType("application/json;charset=UTF-8"))
+        .andExpect(status().isConflict())
+        .andExpect(status().is4xxClientError())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] /updateCountry is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
   }
   // end region scenario for updateCountry //
 
@@ -211,6 +375,26 @@ public class TestMockMvcCountriesController {
     assertEquals(responseMessageString,actualResponse.getResponse().getContentAsString());
     then(countryService).should(times(1)).deleteCountry(id);
     assertThat(output).contains("[FLO] deleteCountry/{countryId} is called");
+  }
+
+  @Test
+  @ExtendWith(OutputCaptureExtension.class)
+  public void deleteCountryException_WillReturnCorrectResponse(CapturedOutput output) throws Exception {
+    // Given
+    EasyRandom easyRandom = new EasyRandom();
+    Integer id = easyRandom.nextInt();
+    given(countryService.deleteCountry(id)).willAnswer( invocation -> { throw new Exception(); });
+
+    // When
+    MvcResult actualResponse = this.mockMvc.perform(delete("/deleteCountry/{countryId}",id))
+        .andExpect(status().isConflict())
+        .andExpect(status().is4xxClientError())
+        .andDo(print())
+        .andReturn();
+
+    // Then
+    assertThat(output).contains("[FLO] deleteCountry/{countryId} is called");
+    assertEquals("",actualResponse.getResponse().getContentAsString());
   }
   // end region scenario for deleteCountry //
 }
